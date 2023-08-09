d5a28eaab6e767721e6fa42cd994dc509a6df62b
haazz <gkwldnjs121@gmail.com>
2023-08-09 12:04:05 +0900

    init project

---
+++ Today/Today/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 하지원 on 2023/08/08.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
